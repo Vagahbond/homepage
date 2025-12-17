06426bd24101ffe35eae50f9da2e07cb72ff596f
Yoni FIRROLONI <pro@yoni-firroloni.com>
2025-12-17 20:28:51 +1300

    flake: add pg

--- flake.nix
+++ flake.nix
@@ -8,6 +8,12 @@
   outputs = {nixpkgs, ...}: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+    configure = ''
+      psql -h localhost -p 5432 -U $USER -d postgres -c "CREATE ROLE homepage WITH LOGIN CREATEDB REPLICATION;"
+      psql -h localhost -p 5432 -U $USER -d postgres -c "CREATE USER homepage WITH PASSWORD 'password';"
+      psql -h localhost -p 5432 -U $USER -d postgres -c "GRANT homepage TO homepage;"
+      psql -h localhost -p 5432 -U $USER -d postgres -c "CREATE DATABASE homepage WITH OWNER homepage;";
+    '';
   in {
     nixosModules = {
       default = {
@@ -21,8 +27,19 @@
     };
     devShells.${system} = {
       default = pkgs.mkShell {
-        buildInputs = [pkgs.bun];
+        buildInputs = [pkgs.bun pkgs.postgresql];
         shellHook = ''
+          alias pginit='pg_ctl -D data init;';
+          alias pgstart='pg_ctl -D data -l pglogfile start -o "-k ./"; ';
+          alias pgconfigure=${pkgs.writeScript "pgconfigure" configure};
+
+          echo "pginit init database"
+          echo "pgstart start database"
+          echo "pgconfigure create db and user"
+
+          export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib";
+
+
           echo Now developping my homepage!
         '';
       };
